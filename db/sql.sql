51e312b11c7c955c8fd60fdb858fc5be3e64b6a4
Ibrahim Ahmad Jabar <30390302+baimtheelite@users.noreply.github.com>
2019-08-05 21:10:57 +0700

    Update sql.sql

--- db/sql.sql
+++ db/sql.sql
@@ -1,36 +1,46 @@
-﻿SELECT *,
-  tb_my_hajat_renovasi.nama_konsumen as nama_konsumen_renovasi,
-  tb_my_hajat_sewa.nama_konsumen as nama_konsumen_sewa,
-  tb_my_hajat_wedding.nama_konsumen as nama_konsumen_wedding,
-  tb_my_hajat_franchise.nama_konsumen as nama_konsumen_franchise,
-  tb_my_hajat_lainnya.nama_konsumen as nama_konsumen_lainnya,
+﻿SELECT
+
+  B.nama_konsumen as nama_konsumen_renovasi,
+  C.nama_konsumen as nama_konsumen_sewa,
+  D.nama_konsumen as nama_konsumen_wedding,
+  E.nama_konsumen as nama_konsumen_franchise,
+  F.nama_konsumen as nama_konsumen_lainnya,
   
-  tb_my_hajat_renovasi.jenis_konsumen as jenis_konsumen_renovasi,
-  tb_my_hajat_sewa.jenis_konsumen as jenis_konsumen_sewa,
-  tb_my_hajat_wedding.jenis_konsumen as jenis_konsumen_wedding,
-  tb_my_hajat_franchise.jenis_konsumen as jenis_konsumen_franchise,
-  tb_my_hajat_lainnya.jenis_konsumen as jenis_konsumen_lainnya,
+  B.jenis_konsumen as jenis_konsumen_renovasi,
+  C.jenis_konsumen as jenis_konsumen_sewa,
+  D.jenis_konsumen as jenis_konsumen_wedding,
+  E.jenis_konsumen as jenis_konsumen_franchise,
+  F.jenis_konsumen as jenis_konsumen_lainnya,
+
 CASE
-    WHEN tb_my_hajat.id_renovasi IS NOT NULL THEN "My Hajat Renovasi"
-    WHEN tb_my_hajat.id_sewa IS NOT NULL THEN "My Hajat Sewa"
-    WHEN tb_my_hajat.id_wedding IS NOT NULL THEN "My Hajat Wedding"
-    WHEN tb_my_hajat.id_franchise IS NOT NULL THEN "My Hajat Franchise"
-    WHEN tb_my_hajat.id_myhajat_lainnya IS NOT NULL THEN "My Hajat Lainnya"
+    WHEN A.id_renovasi IS NOT NULL THEN "My Hajat Renovasi"
+    WHEN A.id_sewa IS NOT NULL THEN "My Hajat Sewa"
+    WHEN A.id_wedding IS NOT NULL THEN "My Hajat Wedding"
+    WHEN A.id_franchise IS NOT NULL THEN "My Hajat Franchise"
+    WHEN A.id_myhajat_lainnya IS NOT NULL THEN "My Hajat Lainnya"
 END AS produk
 
-FROM tb_my_hajat
+FROM tb_my_hajat as A
 
-LEFT JOIN tb_my_hajat_renovasi
-ON tb_my_hajat.id_renovasi = tb_my_hajat_renovasi.id_renovasi
+LEFT JOIN tb_my_hajat_renovasi as B
+ON A.id_renovasi = B.id_renovasi
 
-LEFT JOIN tb_my_hajat_sewa
-ON tb_my_hajat.id_sewa = tb_my_hajat_sewa.id_sewa
+LEFT JOIN tb_my_hajat_sewa as C
+ON A.id_sewa = C.id_sewa
 
-LEFT JOIN tb_my_hajat_wedding
-ON tb_my_hajat.id_wedding = tb_my_hajat_wedding.id_wedding
+LEFT JOIN tb_my_hajat_wedding as D
+ON A.id_wedding = D.id_wedding
 
-LEFT JOIN tb_my_hajat_franchise
-ON tb_my_hajat.id_franchise = tb_my_hajat_franchise.id_franchise
+LEFT JOIN tb_my_hajat_franchise as E
+ON A.id_franchise = E.id_franchise
 
-LEFT JOIN tb_my_hajat_lainnya
-ON tb_my_hajat.id_myhajat_lainnya = tb_my_hajat_lainnya.id_myhajat_lainnya
+LEFT JOIN tb_my_hajat_lainnya as F
+ON A.id_myhajat_lainnya = F.id_myhajat_lainnya
+
+LEFT JOIN user as U
+ON B.id_user = U.id_user OR C.id_user = U.id_user OR D.id_user = U.id_user OR E.id_user = U.id_user OR F.id_user = U.id_user
+
+
+
+-- SELECT * FROM USER
+-- WHERE email = '812813' OR nik = '812813'
