ea138101257e882d0d14bfe67b6e29ba4364249b
Ibrahim Ahmad <ibrahim.ahmadd98@gmail.com>
2019-08-08 08:26:16 +0700

    ubah konsep lampiran status
    ubah konsep lampiran status, sementara
    - My Ihram
    - My Safar
    - My Talim

--- application/views/my_talim/detail_status_my_talim.php
+++ application/views/my_talim/detail_status_my_talim.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-	<section class="content-header">
+	<section class="content-header mt-4 text-center">
 		<h1>
 			Detail My Ta'lim Tickets
 		</h1>
@@ -7,50 +7,10 @@
 
 	<!-- Main content -->
 	<section class="content">
-		<div class="row">
-			<div class="col-lg-6 col-md-6">
-				<div class="row">
-					<div class="mx-auto">
-						<a id="img-link" target="_blank" href="<?= base_url('uploads/mytalim/' . $data->upload_file1) ?>"><img id="gambar" class="rounded" src="<?= base_url('uploads/mytalim/' . $data->upload_file1) ?>" alt="" max-width="100%" max-height="100%"></a>
-					</div>
-				</div>
-				<div class="row mt-1">
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file1 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file1 != NULL ? 'uploads/mytalim/' . $data->upload_file1 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 1">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file2 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file2 != NULL ? 'uploads/mytalim/' . $data->upload_file2 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 2">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file3 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file3 != NULL ? 'uploads/mytalim/' . $data->upload_file3 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 3">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file4 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file4 != NULL ? 'uploads/mytalim/' . $data->upload_file4 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 4">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file5 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file5 != NULL ? 'uploads/mytalim/' . $data->upload_file5 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 5">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file6 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file6 != NULL ? 'uploads/mytalim/' . $data->upload_file6 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 6">
-					</div>
-				</div>
-				<div class="row mt-1">
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file7 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file7 != NULL ? 'uploads/mytalim/' . $data->upload_file7 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 1">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file8 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file8 != NULL ? 'uploads/mytalim/' . $data->upload_file8 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 2">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file9 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file9 != NULL ? 'uploads/mytalim/' . $data->upload_file9 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 3">
-					</div>
-					<div class="col-lg-2 col-md-2 col-sm-2 col-2">
-						<img class="img-thumbnail <?= $data->upload_file10 != NULL ? 'thumb' : '' ?>" src="<?= base_url($data->upload_file10 != NULL ? 'uploads/mytalim/' . $data->upload_file10 : 'assets2/img/no-pict.png') ?>" alt="" max-width="100%" max-height="100%" data-toggle="tooltip" data-placement="top" title="File 4">
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 col-md-6 p-0">
-				<form method="post" action="<?= base_url('ticket_register/edit') ?>" enctype="multipart/form-data">
+		<form method="post" action="<?= base_url('ticket_register/edit') ?>" enctype="multipart/form-data">
+			<div class="row">
+				<!-- Formulir -->
+				<div class="col-lg-6 col-md-6 p-0">
 
 					<!-- Form Pertanyaan My Ta'lim -->
 					<div class="card">
@@ -203,52 +163,64 @@
 							</table>
 						</div>
 					</div>
+
+				</div>
+				<!-- Bagian Munculin lampiran -->
+				<div class="col-lg-6 col-md-6">
 					<!-- Form Upload Lampiran -->
-					<div id="upload" class="card card-primary mt-4">
-						<div class="card-header with-border">
-							<h3 class="card-title">Upload File</h3>
+					<div id="upload" class="card">
+						<div class="card-header">
+							<h3 class="card-title text-center">Lampiran (Attachment)</h3>
 						</div>
-						<div class="card-body" id="dynamic-field">
-							<div class="form-group">
-								<label for="upload_file1">Upload Berkas 1</label>
-								<input name="upload_file1" id="upload_file1" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file2">Upload Berkas 2</label>
-								<input name="upload_file2" id="upload_file2" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file3">Upload Berkas 3</label>
-								<input name="upload_file3" id="upload_file3" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file4">Upload Berkas 4</label>
-								<input name="upload_file4" id="upload_file4" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file5">Upload Berkas 5</label>
-								<input name="upload_file5" id="upload_file5" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file6">Upload Berkas 6</label>
-								<input name="upload_file6" id="upload_file6" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file7">Upload Berkas 7</label>
-								<input name="upload_file7" id="upload_file7" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file8">Upload Berkas 8</label>
-								<input name="upload_file8" id="upload_file8" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file9">Upload Berkas 9</label>
-								<input name="upload_file9" id="upload_file9" type="file" class="form-control enable col-10" disabled>
-							</div>
-							<div class="form-group">
-								<label for="upload_file10">Upload Berkas 10</label>
-								<input name="upload_file10" id="upload_file10" type="file" class="form-control enable col-10" disabled>
-							</div>
+						<div class="card-body p-0" id="dynamic-field">
+							<table class="table text-center" width="100%">
+								<thead>
+									<th>File Terlampir</th>
+									<th>Ubah/tambah file lampiran</th>
+								</thead>
+								<tbody>
+									<tr>
+										<td><?php if ($data->upload_file1 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file1) ?>"><?= $data->upload_file1 ?></a></td><?php } ?>
+										<td><input name="upload_file1" id="upload_file1" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file2 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file2) ?>"><?= $data->upload_file2 ?></a></td><?php } ?>
+										<td><input name="upload_file2" id="upload_file2" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file3 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file3) ?>"><?= $data->upload_file3 ?></a></td><?php } ?>
+										<td><input name="upload_file3" id="upload_file3" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file4 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file4) ?>"><?= $data->upload_file4 ?></a></td><?php } ?>
+										<td><input name="upload_file4" id="upload_file4" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file5 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file5) ?>"><?= $data->upload_file5 ?></a></td><?php } ?>
+										<td><input name="upload_file5" id="upload_file5" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file6 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file6) ?>"><?= $data->upload_file6 ?></a></td><?php } ?>
+										<td><input name="upload_file6" id="upload_file6" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file7 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file7) ?>"><?= $data->upload_file7 ?></a></td><?php } ?>
+										<td><input name="upload_file7" id="upload_file7" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file8 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file8) ?>"><?= $data->upload_file8 ?></a></td><?php } ?>
+										<td><input name="upload_file8" id="upload_file8" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file9 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file9) ?>"><?= $data->upload_file9 ?></a></td><?php } ?>
+										<td><input name="upload_file9" id="upload_file9" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+									<tr>
+										<td><?php if ($data->upload_file10 != NULL) { ?><a target="_blank" class="btn btn-info" href="<?= base_url('uploads/mytalim/' . $data->upload_file10) ?>"><?= $data->upload_file10 ?></a></td><?php } ?>
+										<td><input name="upload_file10" id="upload_file10" type="file" class="form-control enable col-12" disabled></td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 						<?php if (($this->session->userdata('level') == 1) && ($data->id_approval == 0 || $data->id_approval == 1)) { ?>
 							<div class="card-footer text-center">
@@ -264,9 +236,9 @@
 							</div>
 						<?php } ?>
 					</div>
-				</form>
+				</div>
 			</div>
-		</div>
+		</form>
 
 		<!-- Post Komentar -->
 		<div class="row mt-4">
